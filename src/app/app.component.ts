2d90c00e15ac40c4c4bca40ce1e9b657d46bfb8c
martinrusnak365 <martin.rusnak@365.bank>
2024-08-29 16:35:50 +0200

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ChildComponent} from "../components/child/child.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ChildComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'untitled6';
+  data = [
+    {
+    name: 'name 1',
+    description: 'description',
+    uri: 'uri',
+  },
+  {
+    name: 'second name',
+    description: 'description',
+    uri: 'uri',
+  },
+  {
+    name: 'third name',
+    description: 'description',
+    uri: 'uri',
+  }]
 }
---
+++ src/components/child/child.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.css'
+})
+export class ChildComponent {
+  @Input()name: string= '';
+  @Input()description: string= '';
+  @Input()uri: string='';
+
+}
